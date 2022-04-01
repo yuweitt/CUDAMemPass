9635c53408d4a73e5360291163db38153a090f23
yuweitt <unimaybe4@gmail.com>
2022-04-01 11:08:23 +0800

    Skip first run

--- gd.py
+++ gd.py
@@ -12,44 +12,34 @@ import subprocess
 import matplotlib.pyplot as plt
 
 def main():
-    # PREFETCH_SIZE = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]
-    DEVICE_OPTION = [0, 1]
-    ADVICE_OPTION = [0, 1, 3, 5]
-    ADVICE_DEVICE = ["0-0", "0-1", "1-0", "1-1", "3-0", "3-1", "5-0", "5-1"]
-    PREFETCH_DISTANCE = range(74)
-    TARGET_LIST = ADVICE_OPTION
-    average = []
-    fp = open("Prefetch_Distance_Time.txt", "a")
-    for target in TARGET_LIST:
-        for device in DEVICE_OPTION:
-            execution_time = 0
-            make_cmd = "make -f CHAI_Makefile.mk ADVICE={0} DEVICE={1}".format(target, device)
-            # make_cmd = "make -f CHAI_Makefile.mk SIZE={0}".format(target)
-            # make_cmd = "make -f CHAI_Makefile.mk ADVICE={0}".format(target)
-            os.system(make_cmd)
-            # print(make_cmd)
-            for i in range(1, 11):
-                try:
-                    proc = subprocess.Popen(['./main'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-                except:
-                    continue
-                result = proc.stdout.read().decode('utf-8')
-                if("error" in result):
-                    continue
-                time = re.findall("\d+\.\d+", result)
-                print(time[-1])
-                execution_time += float(time[-1])
-            average.append(execution_time/10)
-            print(target, execution_time/10, file = fp)
-            print(target, execution_time/10)
-    fp.close()
+    total_execution_time = 0
+    HtoD = 0
+    Exec = 0
+    DtoH = 0
+    for i in range(0, 11):
+        # try:
+        proc = subprocess.Popen(['./run'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        # except:
+        #     continue
+        if i == 0:
+            continue
+        result = proc.stdout.read().decode('utf-8')
+        time = re.findall("\d+\.\d+", result)
+        print(time[-1])
+        total_execution_time += float(time[-1])
+        # HtoD += float(time[2])
+        # Exec += float(time[3])
+        # DtoH += float(time[4])
+    # average.append(execution_time/10)
+    # print("\t Total", total_execution_time/10, file = fp)
+    print("\t Total", total_execution_time/10)
 
-    x = list(map(str, ADVICE_DEVICE))
-    plt.bar(x, average)
-    plt.title('Device')
-    plt.ylabel('Execution Time')
-    plt.xlabel('Device')
-    plt.savefig('Device_time.png')
+    # x = list(map(str, ADVICE_DEVICE))
+    # plt.bar(x, average)
+    # plt.title('Device')
+    # plt.ylabel('Execution Time')
+    # plt.xlabel('Device')
+    # plt.savefig('Device_time.png')
 
 if __name__ == '__main__':
     main()
--- tuner.py
+++ tuner.py
@@ -19,20 +19,25 @@ from opentuner.search import manipulator
 from opentuner import MeasurementInterface
 from opentuner import Result
 
-EXE_MIN = 9999
+EXE_MIN = 999999
+FILE_DATE = ""
 PY_CMD = ""
-cudaVariableNum = 7 - 1
+cudaVariableNum = 3
+kernelVariableNum = 3
+DEVICE = ["0", "cudaCpuDeviceId"]
 ADVICE_NUM = ['advice_{}'.format(i) for i in range(0, cudaVariableNum)]
+ADVICE_DEVICE = ['device_{}'.format(i) for i in range(0, cudaVariableNum)]
 PREFETCH_SIZE = ['prefetch_{}'.format(i) for i in range(0, cudaVariableNum)]
-PREFETCH_DISTANCE = ['distance_{}'.format(i) for i in range(0, cudaVariableNum)]
-# ADVICE_OPTION = ['cudaMemAdviseSetReadMostly', 'cudaMemAdviseSetPreferredLocation', 'cudaMemAdviseSetAccessedBy']
+# PREFETCH_DISTANCE = ['distance_{}'.format(i) for i in range(0, kernelVariableNum)]
 
 # From https://github.com/jansel/opentuner/blob/master/examples/mario/mario.py
 def call_time():
     try:
         total_execution_time = 0
-        for i in range(1, 11):
+        for i in range(0, 11):
             proc = subprocess.Popen(['./run'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            if i == 0:
+                continue
             result = proc.stdout.read().decode('utf-8')
             time = re.findall("\d+\.\d+", result)
             print(time[-1])
@@ -40,7 +45,15 @@ def call_time():
         return total_execution_time/10
     except:
         print("\t Failed to execute.")
-        return 9999
+        return 999999
+
+def main():
+    global FILE_DATE
+    dt = datetime.datetime.today()
+    FILE_DATE = dt.strftime("%Y_%m_%d_%H-%M-")
+    dirName = "time/"
+    if not os.path.exists(dirName):
+        os.mkdir(dirName)
 
 class PrefetchTuner(MeasurementInterface):
     def manipulator(self):
@@ -49,18 +62,14 @@ class PrefetchTuner(MeasurementInterface):
         ConfigurationManipulator
         """
         m = opentuner.ConfigurationManipulator()
+        for idx in ADVICE_DEVICE:
+            m.add_parameter(manipulator.IntegerParameter(idx, 0, 1))
         for idx in ADVICE_NUM:
             m.add_parameter(manipulator.IntegerParameter(idx, 0, 3))
         for idx in PREFETCH_SIZE:
             m.add_parameter(manipulator.PowerOfTwoParameter(idx, 16, 65536))
-        for idx in PREFETCH_DISTANCE:
-            m.add_parameter(manipulator.IntegerParameter(idx, 0, 20))
-        # m.add_parameter(
-        #     manipulator.IntegerParameter('MEM_ADVICE', 0, 2))
-        # m.add_parameter(
-        #     manipulator.IntegerParameter('PREFETCH_DISTANCE', 1, 68))
-        # m.add_parameter(
-        #     manipulator.PowerOfTwoParameter('PREFETCH_SIZE', 4, 65536))
+        # for idx in PREFETCH_DISTANCE:
+        m.add_parameter(manipulator.IntegerParameter('PREFETCH_DISTANCE', 0, 10))
         return m
 
     def run(self, desired_result, input, limit):
@@ -69,59 +78,46 @@ class PrefetchTuner(MeasurementInterface):
         return performance
         //////////////////////////////////////////////////////////////////////////////
         Sample python command
-        python cudaScript.py --advice_num 5_1 --prefetch_num 0_512_20
+        python cudaScript.py --advice_num 5_1_0 --prefetch_num 0_512_20
         Which means
-        advice fifth object, using first advice option
+        advice fifth object, using first advice option, pin to GPU device 0
         prefetch zero'th object, with prefetch size 512 and prfetch distance 20
         """
         global EXE_MIN, PY_CMD
         cfg = desired_result.configuration.data
         python_cmd = 'python cudaScript.py '
-        # python_cmd += '--advice_type {0} '.format(ADVICE_OPTION[cfg['MEM_ADVICE']])
-        # python_cmd += '--prefetch_size {0} '.format(cfg['PREFETCH_SIZE'])
-        for item in ADVICE_NUM:
+        for idx, item in enumerate(ADVICE_NUM):
             if cfg[item] == 0:
                 continue
             else:
-                python_cmd += '--advice_num '
+                python_cmd += ' --advice_num '
                 num = item[-1]
-                python_cmd += '{0} '.format(num + "_" + str(cfg[item]))
+                python_cmd += '{0} '.format(num + "_" + str(cfg[item]) + "_" + str(cfg[ADVICE_DEVICE[idx]]))
         
         for idx, item in enumerate(PREFETCH_SIZE):
             # Don't prefetch
             if cfg[item] == 16:
                 continue
             else:
-                python_cmd += '--prefetch_num '
+                python_cmd += ' --prefetch_num '
                 num = item[-1]
-                python_cmd += '{0} '.format(num + "_" + str(cfg[item]) + "_" + str(cfg[PREFETCH_DISTANCE[idx]]))
+                python_cmd += '{0} '.format(num + "_" + str(cfg[item]) + "_" + str(cfg['PREFETCH_DISTANCE']))
         
-        # for idx in PREFETCH_NUM:
-        #     if cfg[idx] == 'on':
-        #         python_cmd += '--prefetch_num '
-        #         num = idx[-1]
-        #         python_cmd += '{0} '.format(num)
         print(python_cmd)
         os.system(python_cmd)
         os.system('make clean')
-        os.system('make')
-        # gcc_cmd = 'g++ mmm_block.cpp '
-        # gcc_cmd += ' -D{0}={1}'.format('BLOCK_SIZE', cfg['BLOCK_SIZE'])
-        # gcc_cmd += ' -o ./tmp.bin'
-        # make_cmd = 'make -f CHAI \n'
-        # make -f CHAI_Makefile.mk CNT=40 SIZE=4096 ADVICE=1
-        # make_cmd = 'make -f CHAI_Makefile.mk DISTANCE={0} SIZE={1} ADVICE={2}'\
-        #     .format(cfg['PREFETCH_DISTANCE'], cfg['PREFETCH_SIZE'], cfg['MEM_ADVICE'])
+        os.system('make')        
+        # make_cmd = 'make -f CHAI_Makefile.mk DISTANCE={0}'.format(cfg['PREFETCH_DISTANCE'])
+        # os.system('make -f CHAI_Makefile.mk clean')
+        # os.system(make_cmd)
 
-        # make_cmd = 'make -f CHAI_Makefile.mk DISTANCE={0} SIZE={1}'\
-        #     .format(cfg['PREFETCH_DISTANCE'], cfg['PREFETCH_SIZE'])
-        # make_cmd = 'make -f CHAI_Makefile.mk '
-        # make_cmd += 'DISTANCE=' + str(PREFETCH_DISTANCE)
-        # compile_result = self.call_program(run_cmd)
-        # print(python_cmd)
-        # assert compile_result['returncode'] == 0
-
+        dirName = "time/"
+        exeName = dirName + FILE_DATE + "time.txt"
         exe_time = call_time()
+
+        with open(exeName, "a") as f:
+            f.write(str(exe_time) + "\n")
+
         if exe_time < EXE_MIN:
             EXE_MIN = exe_time
             PY_CMD = python_cmd
@@ -132,13 +128,11 @@ class PrefetchTuner(MeasurementInterface):
 
     def save_final_config(self, configuration):
         """called at the end of tuning"""
-        global EXE_MIN, PY_CMD
+        global EXE_MIN, PY_CMD, FILE_DATE
         dirName = 'json/'
         if not os.path.exists(dirName):
             os.mkdir(dirName)
-        dt = datetime.datetime.today()
-        dt_format = dt.strftime("%Y_%m_%d_%H-%M-")
-        config_name = dirName + dt_format + 'config.json'
+        config_name = dirName + FILE_DATE + 'config.json'
         print("Optimal config save to final_config.json:", configuration.data)
         self.manipulator().save_to_file(configuration.data, config_name)
         with open(config_name) as f:
@@ -148,5 +142,6 @@ class PrefetchTuner(MeasurementInterface):
             json.dump(data, f)
 
 if __name__ == '__main__':
+    main()
     argparser = opentuner.default_argparser()
     PrefetchTuner.main(argparser.parse_args())
